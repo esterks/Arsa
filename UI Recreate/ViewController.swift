2f4a7b853a22f81dd61dc279b36e00c11718a059
Zhafira Millenia <zmillenia@yahoo.co.id>
2020-04-10 17:23:49 +0700

    initProject

---
+++ UI Recreate/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  UI Recreate
+//
+//  Created by Zhafira Millenia on 04/03/20.
+//  Copyright © 2020 Zhafira Millenia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mera: UIImageView!
+    @IBOutlet weak var slem: UIImageView!
+    @IBOutlet weak var scoreMera: UILabel!
+    @IBOutlet weak var scoreSlem: UILabel!
+    @IBOutlet weak var leftName: UILabel!
+    @IBOutlet weak var rightName: UILabel!
+    var leftScore = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mera.image = #imageLiteral(resourceName: "Char1Red")
+        
+    }
+    
+    @IBAction func yellowButton1(_ sender: UIButton) {
+        print("Yellow button pressed!🙂")
+        mera.image = #imageLiteral(resourceName: "Char1Yellow")
+        leftName.text = "Kinu"
+    }
+    
+    @IBAction func redButton1(_ sender: UIButton) {
+        mera.image = UIImage(named: "Char1Red.jpg")
+        leftName.text = "Mera"
+    }
+    
+    @IBAction func greenButton1(_ sender: UIButton) {
+        mera.image = #imageLiteral(resourceName: "Char1Green")
+        leftName.text = "Hijo"
+    }
+    
+    @IBAction func redButton2(_ sender: UIButton) {
+        slem.image = #imageLiteral(resourceName: "Char2Red")
+        rightName.text = "Riddy"
+    }
+
+    @IBAction func yellowButton2(_ sender: UIButton) {
+        slem.image = #imageLiteral(resourceName: "Char2Yellow")
+        rightName.text = "Yell"
+    }
+    
+    @IBAction func greenButton2(_ sender: UIButton) {
+        slem.image = #imageLiteral(resourceName: "Char2Green")
+        rightName.text = "Slem"
+    }
+    
+    
+    @IBAction func fightButton(_ sender: UIButton) {
+        leftScore = leftScore + 1
+        print(leftScore)
+        scoreMera.text = "\(leftScore)"
+    }
+
+    
+}
+
